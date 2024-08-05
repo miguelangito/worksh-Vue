77ab39a747d923bf6519912214c3761e680f0786
deiby888 <cardonadeiby88@gmail.com>
2024-08-05 09:12:58 -0400

    commit

---
+++ workshop/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<banner />
+
+</template>
+
+<script setup lang="ts">
+import banner from '@/components/Banner.vue'
+
+</script>
+
+<style>
+
+
+</style>
---
+++ workshop/src/components/Banner.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <section class="banner-section">
+        <form>
+            <div class="search-input">
+            <input 
+                type="text" 
+                required="true"
+            >
+            <span>Search movie</span>
+            </div>
+        </form>
+        <div class="container-banner-img">
+            <img :src="img" alt="header-img">
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import svgImage from '@/assets/images/header-img.svg'
+
+const img = svgImage;
+</script>
+
+<style lang="scss" scoped>
+.banner-section {
+    width: 100%;
+    height: 50vh;
+    background-image: url(../assets/images/banner-bg.png);
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    form {
+        .search-input {
+        
+            }
+        }
+
+
+    }
+
+</style>
